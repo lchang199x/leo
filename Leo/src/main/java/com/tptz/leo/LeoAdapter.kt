ee884dda39e46c00d86f8bc6aae9f44ca6698140
lchang199x <lchang199x@gmail.com>
2021-11-22 22:13:49 +0800

    add leo module

---
+++ Leo/src/main/java/com/tptz/leo/LeoAdapter.kt
@@ -0,0 +1,21 @@
+package com.tptz.leo
+
+import android.view.View
+import androidx.annotation.DrawableRes
+
+interface LeoAdapter {
+    fun loadingView(): View?
+
+    fun emptyView(
+        tips: String?,
+        @DrawableRes
+        resId: Int,
+        listener: OnRefreshListener?
+    ): View?
+
+    fun offlineView(listener: OnRefreshListener?): View?
+}
+
+interface OnRefreshListener {
+    fun onRefresh()
+}
