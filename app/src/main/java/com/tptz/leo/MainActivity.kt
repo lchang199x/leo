22102f6865e337b78949e7dd76e327ac6380b5ac
lchang199x <lchang199x@gmail.com>
2021-11-23 23:26:24 +0800

    修改布局&隐藏状态栏

--- app/src/main/java/com/tptz/leo/MainActivity.kt
+++ app/src/main/java/com/tptz/leo/MainActivity.kt
@@ -2,22 +2,36 @@ package com.tptz.leo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.tptz.leo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.loading.setOnClickListener {
-            handleLoading(binding.rootView)
+            handleLoading(binding.viewGroup)
         }
         binding.empty.setOnClickListener {
-            handleEmpty(binding.rootView)
+            handleEmpty(binding.viewGroup)
         }
         binding.offline.setOnClickListener {
-            handleOffline(binding.rootView)
+            handleOffline(binding.viewGroup)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        ViewCompat.getWindowInsetsController(binding.root)?.let {
+            it.hide(WindowInsetsCompat.Type.navigationBars())
+            it.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
 }
